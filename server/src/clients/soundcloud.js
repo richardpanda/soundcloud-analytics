7f65f868fbcb4a687232649d99885150721b4baf
Richard Ho <richardpanda1993@gmail.com>
2017-06-26 16:17:58 -0700

    Implement fetch user profile by username

--- server/src/clients/soundcloud.js
+++ server/src/clients/soundcloud.js
@@ -1,18 +1,30 @@
 const axios = require('axios');
 
+const Parser = require('../util/parser');
+
 class SoundCloudClient {
   constructor(clientId) {
     this.clientId = clientId;
   }
 
-  fetchUserPageByUserName(userName) {
-    const url = `https://soundcloud.com/${userName}`;
+  fetchUserPageByUsername(username) {
+    const url = `https://soundcloud.com/${username}`;
     return axios.get(url);
   }
 
   fetchUserProfileByUserId(userId) {
     const url = `http://api.soundcloud.com/users/${userId}?client_id=${this.clientId}`;
     return axios.get(url);
+  }
+
+  async fetchUserProfileByUsername(username) {
+    try {
+      const response = await this.fetchUserPageByUsername(username);
+      const userId = Parser.extractUserIdFromUserProfileHtml(response.data);
+      return await this.fetchUserProfileByUserId(userId);
+    } catch (e) {
+      return e;
+    }
   }
 }
 
--- server/test/soundcloud-client.test.js
+++ server/test/soundcloud-client.test.js
@@ -1,14 +1,15 @@
+const fs = require('fs');
 const nock = require('nock');
 
 const SoundCloudClient = require('../src/clients/soundcloud');
 
 const soundCloudClientId = '12345';
-const soundCloudUserId = '123';
-const soundCloudUserName = 'test';
+const soundCloudUserId = '69257219';
+const soundCloudUsername = 'nghtmre';
 const soundCloudClient = new SoundCloudClient(soundCloudClientId);
 
 describe('SoundCloud client tests', () => {
-  test('fetch user profile', () => {
+  test('fetch user profile by user id', () => {
     nock('http://api.soundcloud.com')
       .get(`/users/${soundCloudUserId}`)
       .query({ client_id: soundCloudClientId })
@@ -17,11 +18,28 @@ describe('SoundCloud client tests', () => {
     return soundCloudClient.fetchUserProfileByUserId(soundCloudUserId);
   });
 
-  test('fetch user page', () => {
+  test('fetch user page by username', () => {
     nock('https://soundcloud.com')
-      .get(`/${soundCloudUserName}`)
+      .get(`/${soundCloudUsername}`)
       .reply(200);
 
-    return soundCloudClient.fetchUserPageByUserName(soundCloudUserName);
+    return soundCloudClient.fetchUserPageByUsername(soundCloudUsername);
+  });
+
+  test('fetch user profile by username', () => {
+    fs.readFile('./test/fake-data/user-profile.html', 'utf8', (err, userProfileHtml) => {
+      expect.assertions(1);
+      
+      nock('https://soundcloud.com')
+        .get(`/${soundCloudUsername}`)
+        .reply(200, userProfileHtml)
+
+      nock('http://api.soundcloud.com')
+        .get(`/users/${soundCloudUserId}`)
+        .query({ client_id: soundCloudClientId })
+        .reply(200);
+
+      return expect(soundCloudClient.fetchUserProfileByUsername(soundCloudUsername)).resolves.toBeDefined();
+    });
   });
 });
