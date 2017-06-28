6e3220c45ac8fc7129283364ad27aef3479d9694
Richard Ho <richardpanda1993@gmail.com>
2017-06-28 14:29:58 -0700

    Add failing tests for fetch user profile

--- server/test/soundcloud-client.test.js
+++ server/test/soundcloud-client.test.js
@@ -1,6 +1,6 @@
 const nock = require('nock');
 
-const { id, permalink } = require('./fake-data/user');
+const { id, invalidId, invalidPermalink, permalink } = require('./fake-data/user');
 const { readUserProfilePage } = require('./util/file-reader');
 const SoundCloudClient = require('../src/clients/soundcloud');
 const { clientId } = require('../src/config/soundcloud');
@@ -23,6 +23,19 @@ describe('SoundCloud client tests', () => {
     await expect(soundCloudClient.fetchUserProfileByUserId(id)).resolves.toBeDefined();
   });
 
+  test('fetch user profile with an invalid user id', async () => {
+    expect.assertions(1);
+
+    nock('http://api.soundcloud.com')
+      .get(`/users/${invalidId}`)
+      .query({ client_id: clientId })
+      .reply(404);
+
+    await expect(soundCloudClient.fetchUserProfileByUserId(invalidId))
+      .rejects
+      .toEqual(new Error('User profile page not found.'));
+  });
+
   test('fetch user profile page by user permalink', async () => {
     expect.assertions(1);
 
@@ -31,6 +44,18 @@ describe('SoundCloud client tests', () => {
       .reply(200);
 
     await expect(soundCloudClient.fetchUserProfilePageByUserPermalink(permalink)).resolves.toBeDefined();
+  });
+
+  test('fetch user profile with an invalid user permalink', async () => {
+    expect.assertions(1);
+
+    nock('https://soundcloud.com')
+      .get(`/${invalidPermalink}`)
+      .reply(404);
+
+    await expect(soundCloudClient.fetchUserProfilePageByUserPermalink(permalink))
+      .rejects
+      .toEqual(new Error('User profile page not found.'));
   });
 
   test('fetch user profile by user permalink', async () => {
