6e0f45f83bc927ddccbe2d517f753aac5657510f
Richard Ho <richardpanda1993@gmail.com>
2017-06-30 15:33:13 -0700

    Add assertions

--- server/test/models/user.test.js
+++ server/test/models/user.test.js
@@ -9,32 +9,48 @@ describe('User model tests', () => {
   });
 
   test('create user given id and permalink', async () => {
+    expect.assertions(1);
+
     await expect(User.create({ id, permalink }))
       .resolves
       .toBeDefined();
   });
 
   test('cannot create user without an id', async () => {
+    expect.assertions(1);
+
     await expect(User.create({ permalink }))
       .rejects
       .toBeDefined();
   });
 
   test('cannot create user without a permalink', async () => {
+    expect.assertions(1);
+
     await expect(User.create({ id }))
       .rejects
       .toBeDefined();
   });
 
   test('cannot create user with duplicate id', async () => {
-    await User.create({ id, permalink });
+    expect.assertions(2);
+
+    await expect(User.create({ id, permalink }))
+      .resolves
+      .toBeDefined();
+
     await expect(User.create({ id, permalink: 'different permalink' }))
       .rejects
       .toBeDefined();
   });
 
   test('cannot create user with duplicate permalink', async () => {
-    await User.create({ id, permalink });
+    expect.assertions(2);
+
+    await expect(User.create({ id, permalink }))
+      .resolves
+      .toBeDefined();
+
     await expect(User.create({ id: 9876, permalink }))
       .rejects
       .toBeDefined();
