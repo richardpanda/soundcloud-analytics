9ea1c41ddeb2a9e26940f5ed446c4404ed7882a6
Richard Ho <richardpanda1993@gmail.com>
2017-07-02 15:06:41 -0700

    Sync models

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,13 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import logger from 'morgan';
 
+import { postgresClient } from './clients';
 import routes from './routes';
 
 const { LOGGING, SERVER_PORT } = process.env;
 const app = express();
+
+postgresClient.sync();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
